c36631a9f7c1716d420d7759580bc12550a0b463
FENG YU <qq13643873342@gmail.com>
2022-01-26 09:07:58 +0900

    commit

---
+++ app/Http/Controllers/admin/item/ItemController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        return view('admin.item.index');
+    }
+    public function create()
+    {
+        return view('admin.item.create');
+    }
+    public function add()
+    {
+        return redirect()->route('admin.item.index');
+    }
+}
---
+++ app/Models/Item.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'price',
+        'stock',
+        'picture',
+    ];
+
+    protected $garded = [
+        'id',
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+}
